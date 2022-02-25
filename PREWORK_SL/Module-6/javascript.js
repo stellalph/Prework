3ca83387f97d9652b4d419cf4609fd10d30bac00
Stella Ling <stellalph2@gmail.com>
2022-02-25 19:52:37 +1030

    Upload Module 1 to Module 14

---
+++ PREWORK_SL/Module-6/javascript.js
@@ -0,0 +1,23 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.background = "Blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "0px";
+
+});
+
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+});   
